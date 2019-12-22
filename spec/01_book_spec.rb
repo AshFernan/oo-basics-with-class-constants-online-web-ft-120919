f96aecc9c3ab22bf5847ed37d596639bfa22c492
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-22 04:05:44 +0000

    Automatically backed up by Learn

--- spec/01_book_spec.rb
+++ spec/01_book_spec.rb
@@ -1,13 +1,15 @@
-describe 'GENRES' do
-  it 'keeps track of all genres' do
-    genres = ["Thriller", "Science Fiction", "Romance"]
-    genres.each_with_index do |genre, i|
-      book = Book.new("Book_#{i}")
-      book.genre = genre
-    end
- 
-    genres.each do |genre|
-      expect(Book::GENRES).to include(genre)
+describe 'Book' do
+ describe 'GENRES' do
+    it 'keeps track of all genres' do
+      genres = ["Thriller", "Science Fiction", "Romance"]
+      genres.each_with_index do |genre, i|
+        book = Book.new("Book_#{i}")
+        book.genre = genre
+      end
+
+      genres.each do |genre|
+        expect(Book::GENRES).to include(genre)
+      end
     end
   end
 end
