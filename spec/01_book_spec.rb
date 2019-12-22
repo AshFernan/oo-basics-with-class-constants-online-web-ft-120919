5a8e060aa65fecad946125db5fe3c226a182df23
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-22 03:54:24 +0000

    Automatically backed up by Learn

--- spec/01_book_spec.rb
+++ spec/01_book_spec.rb
@@ -8,7 +8,7 @@ describe 'Book' do
       end
 
       genres.each do |genre|
-        expect(Book::GENRES).to include(genre)
+        expect(Book::genres).to include(genre)
       end
     end
   end
